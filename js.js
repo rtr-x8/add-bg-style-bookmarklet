e3c445a3e291c4797c9f1f8593eb7c6523e70dd7
RyutaroMatsuoka <ryu.2.24@hotmail.co.jp>
2019-09-06 13:43:25 +0900

    短縮化

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-(function() {
+(function(d) {
 	var colors = [
 		'255,0,0',
 		'0,255,0',
@@ -6,11 +6,7 @@
 		'255,0,255',
 		'0,255,255',
 		'255,255,0',
-	];
-	var loopTimes = 3;
-	var hierarchy = 1;
-	var styleText = '';
-	var opacity = 0.2;
+	], loopTimes = 3, hierarchy = 1, styleText = '', opacity = 0.2;
 	for (var i = 0; i < loopTimes; i++) {
 		for (var j = 0; j < colors.length; j++) {
 			for (var k = 0; k < hierarchy; k++) {
@@ -22,9 +18,9 @@
 			styleText += ',' + opacity + '); }\n';
 		}
 	}
-	var head = document.head || document.getElementsByTagName('head')[0];
-  var style = document.createElement('style');
+	var head = d.head || d.getElementsByTagName('head')[0];
+  var style = d.createElement('style');
   head.appendChild(style);
   style.type = 'text/css';
-  style.appendChild(document.createTextNode(styleText));
-})();
+  style.appendChild(d.createTextNode(styleText));
+})(document);
